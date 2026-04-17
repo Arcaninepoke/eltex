e04bf95dd774195bcdd0544033e31763a299bb1d
ip314s20 <hd33433333hd@gmail.com>
2026-04-17 18:13:30 +0700

    задание 13 выполнено

--- blog-app/src/app/ui/components/article-form/article-form.ts
+++ blog-app/src/app/ui/components/article-form/article-form.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, Output, SimpleChanges} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-article-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './article-form.html',
   styleUrl: './article-form.scss',
 })
-export class ArticleForm {}
+export class ArticleForm {
+  @Input() editData: any = null;
+
+  @Output() articleSubmit = new EventEmitter<any>();
+  @Output() cancel = new EventEmitter<void>();
+
+  form = new FormGroup({
+    title: new FormControl('', [Validators.required, Validators.minLength(25)]),
+    content: new FormControl('', [Validators.required])
+  });
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['editData'] && this.editData) {
+      this.form.patchValue(
+          {title: this.editData.title, content: this.editData.content});
+    } else if (changes['editData'] && !this.editData) {
+      this.form.reset();
+    }
+  }
+
+  onSubmit() {
+    if (this.form.invalid) return;
+    this.articleSubmit.emit({...this.form.value, id: this.editData?.id});
+
+    this.form.reset();
+  }
+
+  onCancel() {
+    this.form.reset();
+    this.cancel.emit();
+  }
+}
--- blog-app/src/app/ui/components/blog-post-card/blog-post-card.ts
+++ blog-app/src/app/ui/components/blog-post-card/blog-post-card.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-blog-post-card',
@@ -8,4 +8,15 @@ import {Component, Input} from '@angular/core';
 })
 export class BlogPostCard {
   @Input() article!: any;
+
+  @Output() deleteClick = new EventEmitter<number>();
+  @Output() editClick = new EventEmitter<any>();
+
+  onDelete() {
+    this.deleteClick.emit(this.article.id);
+  }
+
+  onEdit() {
+    this.editClick.emit(this.article);
+  }
 }
